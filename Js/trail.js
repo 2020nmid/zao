44a582198382da3fb4af4dbcda1eae73f762c8e9
hongsheng-xu <61302414+hongsheng-xu@users.noreply.github.com>
2020-06-12 22:54:06 +0800

    提交js文件
    提交html依赖的js文件

---
+++ Js/trail.js
@@ -0,0 +1,26 @@
+
+   var nav=document.querySelectorAll(".cho li div");
+   for(let i=0;i<nav.length;i++){
+       nav[i].onmouseenter=function(){
+           nav[i].style.display="block";
+       }
+   }
+   var anav=document.querySelectorAll(".sort b a");
+   for(let i=0;i<anav.length;i++){
+       anav[i].onclick=function(){
+        for(let i=0;i<anav.length;i++){
+            anav[i].style.color="black";
+        }
+           anav[i].style.color="red";
+       }
+   }
+   var alist=document.querySelectorAll(".list li");
+   alist[0].style.background="red";
+   for(let i=0;i<alist.length;i++){
+      alist[i].onclick=function(){
+        for(let i=0;i<alist.length-2;i++){
+            alist[i].style.background="white";
+        }
+        alist[i].style.background="red";
+       }
+   }
